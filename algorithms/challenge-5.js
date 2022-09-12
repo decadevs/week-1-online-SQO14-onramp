27b7fb6f79c7661bcb25e05524aabae23c0fb1d7
ikenwaa <ajohn9353@gmail.com>
2022-09-13 00:13:36 +0100

    Added challenge-5.js solution

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,21 @@
-function filterList(arr) {
- 
- }
+// Implement the function filterList(arr) that takes a list of non-negative integers and 
+// strings and returns a new list with the strings filtered out.
+// 1. Initialize an empty array, newArr
+// 2. Loop through the given array.
+// 3. Check for the data types in the arr
+// 4. Push all int types to the newArr
 
+function filterList(arr) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            newArr.push(arr[i])
+        }
+    }
+    console.log(newArr);
+}
+
+filterList([1,2,'a','b']);
+filterList([1,2,'aasf','1','123',123]);
   module.exports =filterList
