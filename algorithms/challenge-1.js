cfef8b29694fd3e727fea9078c3899ce3e39dc17
Olatunde Rasheed <olatunde336@gmail.com>
2022-09-13 21:22:49 +0100

    frontend and algorithm submission

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,24 @@
 function countTruthy(arr) {
+    let number1 = [6,3,0,30,7]
+    number1.filter((item)=>{
+        if(item == 0){
+            return item;
+            console.log(number1.length);
+        }
+    });
+    
 
 }
+countTruthy([6,3,0,30,7]);
 module.exports = countTruthy
+
+
+
+
+// # Challenge 1
+// Implement a function countTruthy(arr) that takes an array and returns the number of truthy values
+
+// # Example
+// countTruthy([6, 3, 0, 30, 7])  to return 4 
+// countTruthy(['', 3, 0, 30, 7]) to return 3
+// countTruthy(['', 0, 0, undefined, 7])  to return 1
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,14 @@
 function fizzBuzz(input){
-
-  
+    if(input % 3 == 0){
+        console.log('fizz');
+    }else if(input % 5 == 0){
+        console.log('buzz')
+    }else if(input % 3 == 0 && input % 5 == 0){
+        console.log('fizzbuzz')
+    }else{
+        console.log('number not divisible by 3 or 5')
+    }
 }
-
+fizzBuzz([1,2,3,4,5,6,15,30,25,18,45,9,12]);
 
 module.exports = fizzBuzz
