5e8498dffb96a585da5d9578e32170ea2afa9316
shittu wasiu <110758549+Abdulwasiu628@users.noreply.github.com>
2022-10-30 23:59:02 +0100

    Update-challenge1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,15 @@
 function countTruthy(arr) {
+  let count = 0;
+    let value;
+    for(value of arr){
+        
+        if (typeof(value) == "number" && value != 0){
+            count++;
+            
+        }  
+     
+    }
+    return count;
 
 }
 module.exports = countTruthy
