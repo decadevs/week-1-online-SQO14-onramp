ff25e4ad15df105df40709206a556c444f323147
shittu wasiu <110758549+Abdulwasiu628@users.noreply.github.com>
2022-10-31 00:35:32 +0100

    Update challenge-3.js
    Challenge 3

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,4 +1,15 @@
 function sumMix(arr) {
+   let count = 0
+    if(typeof(arr)=='number'){
+        return arr;
+    }
+    else{
+        for(value of arr){
+            count += value;
+        }
+        
+    }
+    return count;
    
  }
 
