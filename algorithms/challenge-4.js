494c7326cb0b4b129eaced0fb1ce1b62bb78fe8e
valentine <vnkume@gmail.com>
2023-06-06 20:11:40 +0100

    "completed"

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,9 @@
 function calculateGrade(marks) {
- 
+ let gradeScore = 0;
+ scoresCount = marks.length;
+for (i = 0; i < marks.length; i++){
+    grade += marks[i];
+    if(average )
+}
  }
   module.exports =calculateGrade
