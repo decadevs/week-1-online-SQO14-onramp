62824d749e2a7a581e09260719b797d5f4c8031a
valentine <vnkume@gmail.com>
2023-06-07 15:55:59 +0100

    adjusted

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,11 +1,11 @@
 function fizzBuzz(input){
-    if(input % 3 === 0 && input % 5 != 0){
+    if(input % 3 == 0 && input % 5 != 0){
         return "fizz";
     }
-    else if (input % 5 === 0 && input % 3 != 0){
+    else if (input % 5 == 0 && input % 3 != 0){
         return "buzz";
     }
-    else if (input % 3 === 0 && input % 5 === 0){
+    else if (input % 3 == 0 && input % 5 == 0){
         return "fizzBuzz";
     }
     else {
